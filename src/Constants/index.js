95075bc4d571d068fb89026abe8e2bcc471a5cfc
Manas0410 <manasshrivastava0410@gmail.com>
2024-09-30 01:57:24 +0530

    info up

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -21,41 +21,6 @@ export const navLinks = [
   },
 ];
 
-export const clientReviews = [
-  {
-    id: 1,
-    name: "Emily Johnson",
-    position: "Marketing Director at GreenLeaf",
-    img: "assets/review1.png",
-    review:
-      "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
-  },
-  {
-    id: 2,
-    name: "Mark Rogers",
-    position: "Founder of TechGear Shop",
-    img: "assets/review2.png",
-    review:
-      "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.",
-  },
-  {
-    id: 3,
-    name: "John Dohsas",
-    position: "Project Manager at UrbanTech ",
-    img: "assets/review3.png",
-    review:
-      "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
-  },
-  {
-    id: 4,
-    name: "Ether Smith",
-    position: "CEO of BrightStar Enterprises",
-    img: "assets/review4.png",
-    review:
-      "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.",
-  },
-];
-
 export const myProjects = [
   {
     title: "Podcastr - AI Podcast Platform",
@@ -284,32 +249,32 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
 export const workExperiences = [
   {
     id: 1,
-    name: "Framer",
-    pos: "Lead Web Developer",
-    duration: "2022 - Present",
+    name: "DePronto Infotech",
+    pos: "Lead Frontend Developer",
+    duration: "2024 - Present",
     title:
-      "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
-    icon: "/assets/framer.svg",
+      "As a Software Developer and Team Lead, I specialized in micro frontend development, optimizing product delivery and performance. I led client POC initiatives, resolved complex technical issues, and managed a team of frontend developers, ensuring successful project delivery. By implementing best practices, I improved both code quality and team productivity.",
+    icon: "/assets/company.png",
     animation: "victory",
   },
   {
     id: 2,
-    name: "Figma",
-    pos: "Web Developer",
-    duration: "2020 - 2022",
+    name: "Wipro",
+    pos: "Software Developer",
+    duration: "2022 - 2024",
     title:
-      "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
-    icon: "/assets/figma.svg",
+      "As a frontend developer, I worked extensively with the MERN stack, leveraging Redux for state management and integrating APIs to implement various features. This experience allowed me to build responsive, dynamic user interfaces while collaborating with backend teams to ensure smooth data flow.",
+    icon: "/assets/company.png",
     animation: "clapping",
   },
   {
     id: 3,
-    name: "Notion",
-    pos: "Junior Web Developer",
-    duration: "2019 - 2020",
+    name: "Coding Bootcamp",
+    pos: "Web Developer",
+    duration: "2020 - 2022",
     title:
-      "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
-    icon: "/assets/notion.svg",
+      "I worked on a real-world MERN stack project as a full stack developer during a coding bootcamp. This involved building both frontend and backend features, integrating MongoDB, Express, React, and Node.js to create a dynamic web application.",
+    icon: "/assets/company.png",
     animation: "salute",
   },
 ];
