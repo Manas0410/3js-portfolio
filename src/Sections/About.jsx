11098f1b27d6aac4c1dd9a48844d250c3d0ff66e
Manas0410 <manasshrivastava0410@gmail.com>
2025-03-13 23:03:32 +0530

    data up

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
             <img
-              src="assets/grid2.png"
+              src="assets/mern.png"
               alt="grid-2"
               className="w-full sm:h-[276px] h-fit object-contain"
             />
